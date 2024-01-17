6adce3b282d1c66bcee329f0185b8f0741e46d52
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2024-01-17 10:44:08 +0530

    RoleID Dynamic Option Creation

--- updateUser/updateUser.js
+++ updateUser/updateUser.js
@@ -1,13 +1,12 @@
 
 
 
-const token = "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJzZWxmIiwic3ViIjoic2siLCJpYXQiOjE3MDU0MTA2MDgsInJvbGVzIjoiUk9MRV9BRE1JTiJ9.zMOnasBmirIVXSOSxSPOqqWttuomlqmY9XLcst22QcS8_n2tcgG4dTgk_aNwVrwcgsNV4_WS3wMdZWMhnEtrP6wgA37d6hB_z5lSJs5MIhefAEYA7aOmu-jfFEcU3G6Chva_j9jbMDxEi7fhSYO1qYP57CR3U-AzDJivwuvuMVwSWUkUwq8f7sWByiIbi9yTTJWQ7NycVIg2pKhQJkcOVKZpa8w1qg8Zf03RT5KRCNPeH_kXmRWWrx9hmEIaJlGtlmcOoPO-zeEWgwLkW5QF0boioQHkZ1kt3EP1AslGocWJQUOjciQ7-aemPhY_kzuMfYEfFGH-4aS2TtadnvcMQQ"; // Replace with your actual bearer token
+const token = "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJzZWxmIiwic3ViIjoic2siLCJpYXQiOjE3MDU0NjgxNjgsInJvbGVzIjoiUk9MRV9BRE1JTiJ9.LqOjbmHG0QTdywj77Il9l1RTP2t0-9Y7el86FXn7Gz6VWAgv74sYgvg9hj9Tqh4sEEdRNGEJYnG4fkIxQKqemzN7pakp8YmWwv2CQdUKWGoOEXF2niIuC0SCE3bXk0dpBAU678oyMu5dWBISOKQCJtYsSTxcBBumKsDE6GXBG9XUNwpU6dOkgpb4nx-88-SEHD2nm-lhENOmG3C8xXaiQBEHH0uGN0ny7GLdyEmfnz29WCc2jBkeXlvojvY_e5WoOajd5n53sg7YRcEbrtoYLr3sL_qQi26oFtAle3EJobDw6D9J56oTb5FIUJrJyecII5dXvEqTf6unsyLjSFr7tQ"; // Replace with your actual bearer token
 
 
-// Fetch status options from the API with a bearer token
-async function fetchStatusOptions() {
+async function fetchRoleIdOptions() {
     
-    const apiUrl = "http://127.0.0.1:8080/api/status";
+    const apiUrl = "http://127.0.0.1:8080/api/roles";
 
     try {
         const response = await fetch(apiUrl, {
@@ -22,82 +21,24 @@ async function fetchStatusOptions() {
             throw new Error(`Failed to fetch status options: ${response.status}`);
         }
 
-        const statusData = await response.json();
-        const statusSelect = document.getElementById("status");
+        const roleData = await response.json();
+        const roleSelect = document.getElementById("roleId");
 
         // Populate the status dropdown with options from the API
-        statusData.forEach(status => {
+        roleData.forEach(role => {
             const option = document.createElement("option");
-            option.value = status.statusId;
-            option.textContent = status.statusLevel;
-            statusSelect.appendChild(option);
+            option.value = role.roleId;
+            option.textContent = role.designation;
+            roleSelect.appendChild(option);
         });
     } catch (error) {
         console.error("Error fetching status options:", error);
     }
 }
 
-// Call the function to fetch status options when the page loads
-fetchStatusOptions();
 
+fetchRoleIdOptions();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Fetch status options from the API with a bearer token
-async function fetchPriorityOptions() {
-    
-    const apiUrl = "http://127.0.0.1:8080/api/priorities";
-
-    try {
-        const response = await fetch(apiUrl, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to fetch status options: ${response.status}`);
-        }
-
-        const priorityData = await response.json();
-        const prioritySelect = document.getElementById("priority");
-
-        // Populate the status dropdown with options from the API
-        priorityData.forEach(priority => {
-            const option = document.createElement("option");
-            option.value = priority.priorityId;
-            option.textContent = priority.priorityStatus;
-            prioritySelect.appendChild(option);
-        });
-    } catch (error) {
-        console.error("Error fetching status options:", error);
-    }
-}
-
-// Call the function to fetch status options when the page loads
-fetchPriorityOptions();
 
 
 
