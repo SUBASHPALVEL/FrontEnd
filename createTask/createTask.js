88935cf2295a20eba2a93f34fcdbdcfc98cff600
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2024-01-18 17:40:25 +0530

    create task  error

--- createTask/createTask.js
+++ createTask/createTask.js
@@ -1,10 +1,28 @@
+const formContainer = document.getElementById("taskCreationForm");
 const successMessage = document.getElementById("successMessage");
 const failureMessage = document.getElementById("failureMessage");
+
+const token = localStorage.getItem('token');
 
 let createdDate= new Date().toISOString().split("T")[0];
 let completedDate= null;
 
-function createTask() {
+let statusOptionsFetched = false;
+let priorityOptionsFetched = false;
+
+if (!statusOptionsFetched) {
+  fetchStatusOptions();
+  statusOptionsFetched = true;
+}
+
+if (!priorityOptionsFetched) {
+  fetchPriorityOptions();
+  priorityOptionsFetched = true;
+}
+
+
+function createTask(event) {
+  event.preventDefault();
   const apiUrl = "http://127.0.0.1:8080/api/tasks";
   const title = document.getElementById("title").value;
   const description = document.getElementById("description").value;
@@ -13,30 +31,18 @@ function createTask() {
   const dueDate = document.getElementById("dueDate").value;
   const assignedUsersInput = document.getElementById("assignedUsers").value;
 
-  // Validate assignedUsers input
   const assignedUsers = validateAssignedUsers(assignedUsersInput);
   if (!assignedUsers) {
-    // Show an error message and return from the function
+    
     showFor4SecondsForFailure();
     console.log("NO ASSIGNED");
     
   } else 
-    //   requestBody = {
-    //   title: title,
-    //   description: description,
-    //   status: status,
-    //   priority: priority,
-    //   dueDate: dueDate,
-    //   createdDate: new Date().toISOString().split("T")[0],
-    //   completedDate: null,
-    //   assignedUsers: assignedUsers,
-    // };
-
-    // console.log(requestBody);
 
     fetch(apiUrl, {
       method: "POST",
       headers: {
+        'Authorization': `Bearer ${token}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ title, description,status ,
@@ -57,11 +63,95 @@ function createTask() {
       });
   }
 
+
+
+
+
+
+
+
+
+  async function fetchStatusOptions() {
+    const apiUrl = "http://127.0.0.1:8080/api/status";
+  
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch status options: ${response.status}`);
+      }
+  
+      const statusData = await response.json();
+      const statusSelect = document.getElementById("status");
+  
+      // Populate the status dropdown with options from the API
+      statusData.forEach((status) => {
+        const option = document.createElement("option");
+        option.value = status.statusId;
+        option.textContent = status.statusLevel;
+        statusSelect.appendChild(option);
+      });
+    } catch (error) {
+      console.error("Error fetching status options:", error);
+    }
+  }
+
+
+
+
+
+
+
+
+
+  async function fetchPriorityOptions() {
+    const apiUrl = "http://127.0.0.1:8080/api/priorities";
+  
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch status options: ${response.status}`);
+      }
+  
+      const priorityData = await response.json();
+      const prioritySelect = document.getElementById("priority");
+  
+      // Populate the status dropdown with options from the API
+      priorityData.forEach((priority) => {
+        const option = document.createElement("option");
+        option.value = priority.priorityId;
+        option.textContent = priority.priorityStatus;
+        prioritySelect.appendChild(option);
+      });
+    } catch (error) {
+      console.error("Error fetching status options:", error);
+    }
+  }
+
+
+
+
+
+
+
+
+
 function validateAssignedUsers(input) {
-  // Split the input into an array of user IDs
   const userIDs = input.split(",").map((id) => parseInt(id.trim()));
 
-  // Check if any user ID is not a valid number
   if (userIDs.some(isNaN)) {
     return null;
   }
@@ -76,7 +166,7 @@ function showFor4SecondsForSuccess() {
     successMessage.style.display = "none";
     formContainer.style.opacity = "1";
     resetForm();
-    window.location.href = "";
+    window.location.href = "../homepage/homepage.html";
   }, 4000);
 }
 
@@ -92,3 +182,7 @@ function showFor4SecondsForFailure() {
 function resetForm() {
   document.getElementById("taskCreationForm").reset();
 }
+
+function handleCancel() {
+  window.location.href = "../homepage/homepage.html";
+  }
--- updateTask/updateTask.js
+++ updateTask/updateTask.js
@@ -6,242 +6,220 @@ let TaskDetailsFetched = false;
 const formContainer = document.getElementById("taskUpdateForm");
 const successMessage = document.getElementById("successMessage");
 const failureMessage = document.getElementById("failureMessage");
+let previousPage = document.referrer;
 
 if (!statusOptionsFetched) {
-    fetchStatusOptions();
-    statusOptionsFetched = true;
-  }
+  fetchStatusOptions();
+  statusOptionsFetched = true;
+}
 
-  if (!priorityOptionsFetched) {
-    fetchPriorityOptions();
-    priorityOptionsFetched = true;
-  }
+if (!priorityOptionsFetched) {
+  fetchPriorityOptions();
+  priorityOptionsFetched = true;
+}
 
-  if (!TaskDetailsFetched) {
-    fetchTaskDetails(updateTaskId);
-    TaskDetailsFetched = true;
-  }
-
+if (!TaskDetailsFetched) {
+  fetchTaskDetails(updateTaskId);
+  TaskDetailsFetched = true;
+}
 
 async function fetchStatusOptions() {
-    
-    const apiUrl = "http://127.0.0.1:8080/api/status";
+  const apiUrl = "http://127.0.0.1:8080/api/status";
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        });
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`Failed to fetch status options: ${response.status}`);
-        }
-
-        const statusData = await response.json();
-        const statusSelect = document.getElementById("status");
-
-        // Populate the status dropdown with options from the API
-        statusData.forEach(status => {
-            const option = document.createElement("option");
-            option.value = status.statusId;
-            option.textContent = status.statusLevel;
-            statusSelect.appendChild(option);
-        });
-    } catch (error) {
-        console.error("Error fetching status options:", error);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch status options: ${response.status}`);
     }
-}
 
+    const statusData = await response.json();
+    const statusSelect = document.getElementById("status");
+
+    // Populate the status dropdown with options from the API
+    statusData.forEach((status) => {
+      const option = document.createElement("option");
+      option.value = status.statusId;
+      option.textContent = status.statusLevel;
+      statusSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Error fetching status options:", error);
+  }
+}
 
 async function fetchPriorityOptions() {
-    
-    const apiUrl = "http://127.0.0.1:8080/api/priorities";
+  const apiUrl = "http://127.0.0.1:8080/api/priorities";
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        });
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`Failed to fetch status options: ${response.status}`);
-        }
-
-        const priorityData = await response.json();
-        const prioritySelect = document.getElementById("priority");
-
-        // Populate the status dropdown with options from the API
-        priorityData.forEach(priority => {
-            const option = document.createElement("option");
-            option.value = priority.priorityId;
-            option.textContent = priority.priorityStatus;
-            prioritySelect.appendChild(option);
-        });
-    } catch (error) {
-        console.error("Error fetching status options:", error);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch status options: ${response.status}`);
     }
+
+    const priorityData = await response.json();
+    const prioritySelect = document.getElementById("priority");
+
+    // Populate the status dropdown with options from the API
+    priorityData.forEach((priority) => {
+      const option = document.createElement("option");
+      option.value = priority.priorityId;
+      option.textContent = priority.priorityStatus;
+      prioritySelect.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Error fetching status options:", error);
+  }
 }
-
-
 
 // Function to update task details
 async function updateTask(event) {
-    event.preventDefault(); 
+  event.preventDefault();
 
-   const apiUrl = `http://127.0.0.1:8080/api/tasks/${updateTaskId}`; 
+  const apiUrl = `http://127.0.0.1:8080/api/tasks/${updateTaskId}`;
 
-   const assignedUsersArray= document.getElementById("assignedUsers").value.split(",").map(user => user.trim());
+  const assignedUsersArray = document
+    .getElementById("assignedUsers")
+    .value.split(",")
+    .map((user) => user.trim());
 
-   const priorityString = document.getElementById("priority").value;
-   const priorityObject = { "priorityId": parseInt(priorityString, 10) };
+  const priorityString = document.getElementById("priority").value;
+  const priorityObject = { priorityId: parseInt(priorityString, 10) };
 
-   const statusString = document.getElementById("status").value;
-   const statusObject = { "statusId": parseInt(statusString, 10) }; 
+  const statusString = document.getElementById("status").value;
+  const statusObject = { statusId: parseInt(statusString, 10) };
 
-   const today = new Date();
-    const modifiedDate = today.toISOString().split('T')[0];
+  const today = new Date();
+  const modifiedDate = today.toISOString().split("T")[0];
 
+  const updatedTask = {
+    title: document.getElementById("title").value,
+    description: document.getElementById("description").value,
 
-   
-    const updatedTask = {
-        title: document.getElementById("title").value,
-        description: document.getElementById("description").value,
+    status: statusObject,
+    priority: priorityObject,
 
+    modifiedDate: modifiedDate,
 
+    dueDate: document.getElementById("dueDate").value,
+    completedDate: document.getElementById("completedDate").value,
+    assignedUsers: assignedUsersArray.map((userId) => {
+      return { userId: parseInt(userId) };
+    }),
+  };
 
-        status: statusObject,
-        priority: priorityObject,
+  console.log(updatedTask);
 
-        modifiedDate: modifiedDate,
-
-
-
-        dueDate: document.getElementById("dueDate").value,
-        completedDate: document.getElementById("completedDate").value,
-        assignedUsers: assignedUsersArray.map(userId => {
-            return { "userId": parseInt(userId) };
-          })
-    };
-
-    console.log(updatedTask);
-
-
-
-
-    try {
-            fetch (apiUrl, {
-            method: "PUT",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(updatedTask)
-        }).then(response =>{if (!response.ok) {
-            console.log(response.status);
-            showFor4SecondsForFailure();
-            throw new Error(`Failed to update task: ${response.status}`);
-        }
-
-       
-        console.log("Task updated successfully:");
-        showFor4SecondsForSuccess();
-        
-
-
-     
-    
-    
-    } ) } catch (error) {
-        console.error("Error updating task:", error);
+  try {
+    fetch(apiUrl, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    }).then((response) => {
+      if (!response.ok) {
+        console.log(response.status);
         showFor4SecondsForFailure();
-      
-    }
+        throw new Error(`Failed to update task: ${response.status}`);
+      }
 
+      console.log("Task updated successfully:");
+      showFor4SecondsForSuccess();
+    });
+  } catch (error) {
+    console.error("Error updating task:", error);
+    showFor4SecondsForFailure();
+  }
 }
-
-
-
-
-
 
 async function fetchTaskDetails(updateTaskId) {
-   
-    const apiUrl = `http://127.0.0.1:8080/api/tasks/${updateTaskId}`;
+  const apiUrl = `http://127.0.0.1:8080/api/tasks/${updateTaskId}`;
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        });
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`Failed to fetch task details: ${response.status}`);
-        }
-
-        const taskData = await response.json();
-
-        // Populate form fields with task details
-        document.getElementById("taskId").value = taskData.taskId;
-        document.getElementById("title").value = taskData.title;
-        document.getElementById("description").value = taskData.description;
-        document.getElementById("status").value = taskData.status.statusId;
-        document.getElementById("priority").value = taskData.priority.priorityId;
-        document.getElementById("dueDate").value = taskData.dueDate;
-        document.getElementById("completedDate").value = taskData.completedDate;
-        document.getElementById("assignedUsers").value = taskData.assignedUsers.map(user => user.userId).join(" ,");
-
-    } catch (error) {
-        console.error("Error fetching task details:", error);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch task details: ${response.status}`);
     }
+
+    const taskData = await response.json();
+
+    // Populate form fields with task details
+    document.getElementById("taskId").value = taskData.taskId;
+    document.getElementById("title").value = taskData.title;
+    document.getElementById("description").value = taskData.description;
+    document.getElementById("status").value = taskData.status.statusId;
+    document.getElementById("priority").value = taskData.priority.priorityId;
+    document.getElementById("dueDate").value = taskData.dueDate;
+    document.getElementById("completedDate").value = taskData.completedDate;
+    document.getElementById("assignedUsers").value = taskData.assignedUsers
+      .map((user) => user.userId)
+      .join(" ,");
+  } catch (error) {
+    console.error("Error fetching task details:", error);
+  }
 }
 
-
 function showFor4SecondsForSuccess() {
-    formContainer.style.opacity = "0.5";
-    successMessage.style.display = "block";
-    setTimeout(() => {
-      successMessage.style.display = "none";
-      formContainer.style.opacity = "1";
-      if (fromYourTask == true) {
-        localStorage.setItem("fromYourTask",false);
-        window.location.href = "../yourTasks/yourTasks.html";
-      }else{
-        window.location.href = "../allTasks/allTasks.html";
-      }
-      
-    }, 4000);
-  }
-  
-  function showFor4SecondsForFailure() {
-    failureMessage.style.display = "block";
-    formContainer.style.opacity = "0.5";
-    setTimeout(() => {
-      failureMessage.style.display = "none";
-      formContainer.style.opacity = "1";
-      if (fromYourTask) {
-        localStorage.setItem("fromYourTask",false);
-        window.location.href = "../yourTasks/yourTasks.html";
-      }else{
-        window.location.href = "../allTasks/allTasks.html";
-      }
-    }, 4000);
-  }
+  formContainer.style.opacity = "0.5";
+  successMessage.style.display = "block";
+  setTimeout(() => {
+    successMessage.style.display = "none";
+    formContainer.style.opacity = "1";
 
+    if (previousPage.includes("yourTasks/yourTasks.html")) {
+      window.location.href = "../yourTasks/yourTasks.html";
+    } else if (previousPage.includes("allTasks/allTasks.html")) {
+      window.location.href = "../allTasks/allTasks.html";
+    } else {
+      window.location.href = "../homepage/homepage.html";
+    }
+  }, 4000);
+}
 
-  function cancelUpdate(){
-    if (fromYourTask) {
-        localStorage.setItem("fromYourTask",false);
-        window.location.href = "../yourTasks/yourTasks.html";
-      }else{
-        window.location.href = "../allTasks/allTasks.html";
-      }
+function showFor4SecondsForFailure() {
+  failureMessage.style.display = "block";
+  formContainer.style.opacity = "0.5";
+  setTimeout(() => {
+    failureMessage.style.display = "none";
+    formContainer.style.opacity = "1";
+    if (previousPage.includes("yourTasks/yourTasks.html")) {
+      window.location.href = "../yourTasks/yourTasks.html";
+    } else if (previousPage.includes("allTasks/allTasks.html")) {
+      window.location.href = "../allTasks/allTasks.html";
+    } else {
+      window.location.href = "../homepage/homepage.html";
+    }
+  }, 4000);
+}
+
+function cancelUpdate() {
+  if (previousPage.includes("yourTasks/yourTasks.html")) {
+    window.location.href = "../yourTasks/yourTasks.html";
+  } else if (previousPage.includes("allTasks/allTasks.html")) {
+    window.location.href = "../allTasks/allTasks.html";
+  } else {
+    window.location.href = "../homepage/homepage.html";
   }
+}
