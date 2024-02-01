449ff7d47aa5ba377e0ecceb7aa49e5cd9489760
subashpalvel <subashpalvel.ai@gmail.com>
2024-02-02 05:03:50 +0530

    Minor Update to include name

--- signUp/signUp.js
+++ signUp/signUp.js
@@ -1,50 +1,13 @@
 let updateUserId;
-const token = localStorage.getItem("token");
 updateUserId = localStorage.getItem("updateUserId");
-let roleIdOptionsFetched = false;
 const formContainer = document.getElementById("newUserForm");
 const successMessage = document.getElementById("successMessage");
 const failureMessage = document.getElementById("failureMessage");
 
-if (!roleIdOptionsFetched) {
-  fetchRoleIdOptions();
-  roleIdOptionsFetched = true;
-}
-
-async function fetchRoleIdOptions() {
-  const apiUrl = "http://127.0.0.1:8080/api/roles";
-
-  try {
-    const response = await fetch(apiUrl, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch status options: ${response.status}`);
-    }
-
-    const roleData = await response.json();
-    const roleSelect = document.getElementById("roleId");
-
-    roleData.forEach((role) => {
-      const option = document.createElement("option");
-      option.value = role.roleId;
-      option.textContent = role.designation;
-      roleSelect.appendChild(option);
-    });
-  } catch (error) {
-    console.error("Error fetching status options:", error);
-  }
-}
-
 async function createUser(event) {
   event.preventDefault();
 
-  const apiUrl = "http://127.0.0.1:8080/api/users";
+  const apiUrl = "http://127.0.0.1:8080/auth/admin";
 
   const roleString = document.getElementById("roleId").value;
   const roleObject = { roleId: parseInt(roleString, 10) };
@@ -87,7 +50,7 @@ function showFor4SecondsForSuccess() {
   setTimeout(() => {
     successMessage.style.display = "none";
     formContainer.style.opacity = "1";
-    window.location.href = "../allUsers/allUsers.html";
+    window.location.href = "../login/login.html";
   }, 4000);
 }
 
@@ -101,5 +64,5 @@ function showFor4SecondsForFailure() {
 }
 
 function handleCancel() {
-  window.location.href = "../homepage/homepage.html";
+  window.location.href = "../login/login.html";
 }
